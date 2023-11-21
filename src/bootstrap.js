b5e7294401213445f0e69b27d207df38b45e5f93
Kshitij Raj <kshitij.raj@kshitij.raj>
2023-11-21 20:57:44 +0530

    updated the exports

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,7 +4,8 @@ import { ThemeProvider } from "@mui/material/styles";
 import CustomTheme from "./themeProvider/Theme";
 import SharedButton from "./components/SharedButton";
 import { useMediaQuery, useTheme } from "@mui/material";
-export { CustomTheme, SharedButton };
+import { WindowEventService, UserInfoType, WindowEvents } from './eventservice/index';
+export { CustomTheme, SharedButton, WindowEventService, UserInfoType, WindowEvents };
 
 const Main = () => {
    const theme = useTheme();
