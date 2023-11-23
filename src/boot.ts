8eabf9351d5683270cbdcb164f946210b389724d
Kshitij Raj <kshitij.raj@kshitij.raj>
2023-11-24 01:02:23 +0530

    boot file created

---
+++ src/boot.ts
@@ -0,0 +1,5 @@
+import CustomTheme from "./themeProvider/Theme";
+import  SharedButton  from "./components/SharedButton";
+export { WindowEventService, WindowEvents } from './eventservice';
+export type { UserInfoType } from './eventservice/index';
+export { CustomTheme,  SharedButton};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import CustomTheme from "./themeProvider/Theme";
-import  SharedButton  from "./components/SharedButton";
-export { WindowEventService, WindowEvents } from './eventservice';
-export type { UserInfoType } from './eventservice/index';
-export { CustomTheme,  SharedButton};
-
-
-
+import("./boot");
